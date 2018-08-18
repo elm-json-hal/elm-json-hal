52276702c9f32080fc33cabf67dc9d47df7c1630
Jason Carr <jcarr250@gmail.com>
2018-08-17 21:24:04 -0400

    Expand tests for Link.decode

--- tests/Link.elm
+++ tests/Link.elm
@@ -2,7 +2,7 @@ module Link exposing (..)
 
 import Expect exposing (Expectation)
 import HAL.Link as Link exposing (Link, empty, fromHref)
-import Json.Decode as Decode
+import Json.Decode as Decode exposing (decodeString)
 import Test exposing (..)
 
 
@@ -13,15 +13,35 @@ suite =
             (List.map
                 (\( x, y, z ) -> makeCase x y z)
                 testCases
+                ++ [ describe "fails on"
+                        [ makeFailCase "empty" ""
+                        , makeFailCase "a number" "2"
+                        , makeFailCase "a string" "\"Hello\""
+                        , makeFailCase "an array" "[]"
+                        , makeFailCase "an empty object" "{}"
+                        , makeFailCase "an bad href" """{ "href": 2 }"""
+                        , makeFailCase "a missing href" """{ "templated": true }"""
+                        ]
+                   ]
             )
         ]
 
 
-makeCase : String -> String -> Link -> Test
+type alias Name =
+    String
+
+
+makeFailCase : Name -> String -> Test
+makeFailCase description input =
+    test description
+        (\_ -> Expect.err (decodeString Link.decode input))
+
+
+makeCase : Name -> String -> Link -> Test
 makeCase description input result =
     test description
         (\_ ->
-            Expect.equal (Decode.decodeString Link.decode input) (Ok result)
+            Expect.equal (decodeString Link.decode input) (Ok result)
         )
 
 
