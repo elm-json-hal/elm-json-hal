b2de6a4d846a51f9459fcf80e6b5c2ad4b6b5dd5
Jason Carr <jcarr250@gmail.com>
2018-08-19 18:33:01 -0400

    Add documentation to HAL.Links

--- src/HAL/Links.elm
+++ src/HAL/Links.elm
@@ -1,10 +1,23 @@
 module HAL.Links exposing (Links, decodeLinks, decodeResourceObject)
 
+{-| This module exposes types and decoders for the colection of links, and HAL Resource Objects
+
+@docs Links
+@docs decodeLinks
+@docs decodeResourceObject
+
+-}
+
 import Dict exposing (Dict)
 import HAL.Link as Link exposing (Link)
 import Json.Decode as Decode exposing (Decoder, bool, field, nullable, string)
 
 
+{-|
+
+    Alias for object of links, a multi-dictionary of individual Link objects
+
+-}
 type alias Links =
     Dict String (List Link)
 
@@ -22,11 +35,24 @@ decodeMultiDict dec =
     Decode.dict (decodeOneOrMany dec)
 
 
+{-|
+
+    Decode the links object, that is, an object containing for each key, a link object or array of link objects.
+
+    Curies are treated as any other link object and not specifically validated.
+
+-}
 decodeLinks : Decoder Links
 decodeLinks =
     decodeMultiDict Link.decode
 
 
+{-|
+
+    Decode an entire Resource object, using a decoder for the resource, as well as a decoder for embedded resources.
+    As decodeLinks, curies are not treated differently than other link objects.
+
+-}
 decodeResourceObject :
     Decoder resource
     -> Decoder embedded
